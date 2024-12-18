99bcfd3d3212de5a0be1538f962050551caa08be
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-18 22:53:58 +0530

    First Commit

---
+++ a.js
@@ -0,0 +1,5 @@
+"use strict";
+function cout(first, last) {
+    console.log("Hello my name is " + first + " " + last);
+}
+cout("Bhanu", "Pratap");
---
+++ a.ts
@@ -0,0 +1,5 @@
+function cout(first: string, last: string){
+    console.log("Hello my name is "+first+" "+last); 
+}
+
+cout("Bhanu", "Pratap"); 
