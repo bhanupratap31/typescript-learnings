16f2a347575b984872a62b8879ec659ac06653c2
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-20 15:51:54 +0530

    Generics

--- src/a.ts
+++ src/a.ts
@@ -1,3 +1,11 @@
+function identity<T> (arg: T): T{
+    return arg; 
+}
+
+let output1 = identity<string>("bhanu"); 
+let output2 = identity<number>(1); 
+
+console.log(output1.toUpperCase());
 // function cout(first: string, last: string){
 //     console.log("Hello my name is "+first+" "+last); 
 // }
@@ -9,30 +17,32 @@
 // }
 
 // console.log(sum(10,20));
-interface User {
-    firstName: string, 
-    lastName: string, 
-    age: number,
-    email?: string 
-};
+// interface User {
+//     firstName: string, 
+//     lastName: string, 
+//     age: number,
+//     email?: string 
+// };
 
-function isLegal(user: User){
-    if(user.age>18) return true; 
-    return false; 
-}
+// function isLegal(user: User){
+//     if(user.age>18) return true; 
+//     return false; 
+// }
 
-function greet(user: User){
-    console.log("Hi" + user.firstName +"!"); 
-}
+// function greet(user: User){
+//     console.log("Hi" + user.firstName +"!"); 
+// }
 
-isLegal({
-    firstName: "Bhanu", 
-    lastName: "Pratap", 
-    age: 26
-});
+// isLegal({
+//     firstName: "Bhanu", 
+//     lastName: "Pratap", 
+//     age: 26
+// });
 
-greet({
-    firstName: "Bhanu", 
-    lastName: "Pratap", 
-    age: 26
-});
+// greet({
+//     firstName: "Bhanu", 
+//     lastName: "Pratap", 
+//     age: 26
+// });
+
+
